8b65239d1a0029462878be928bc606cb7d8ab190
github-Tanyap1 <github-Tanya.Pachkalov@MyGeorgian.ca>
2022-04-21 14:16:07 -0400

    fileupload

--- fileupload/app.js
+++ fileupload/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const upload = require('express-fileupload');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
+var index= require('./controllers/index');
+var users = require('./controllers/users');
 
 var app = express();
 
@@ -19,8 +22,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
+
+
+
+app.use(upload());
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/')
+})
+
+app.post('/', (req,res)=>{
+if (req.files){
+  console.log(req.files)
+  var file =req.files.file
+  var filename=file.name
+  console.log(filename)
+
+  file.mv('./public/images/'+filename,function(err){
+    if(err){
+      res.send(err)
+    }else{
+      res.send("File Uploaded")
+    }
+  })
+}
+
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- fileupload/controllers/index.js
+++ fileupload/controllers/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const upload = require('express-fileupload')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'fileupload' });
